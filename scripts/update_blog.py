8875baf08fae1b26415d304065c13dd528177675
jihoon <163746946+hoonZeee@users.noreply.github.com>
2025-03-18 10:58:06 +0900

    Update update_blog.py

--- scripts/update_blog.py
+++ scripts/update_blog.py
@@ -3,7 +3,6 @@ import git
 import os
 
 # 벨로그 RSS 피드 URL
-# example : rss_url = 'https://api.velog.io/rss/@rimgosu'
 rss_url = 'https://api.velog.io/rss/@hoonzeee'
 
 # 깃허브 레포지토리 경로
@@ -19,27 +18,26 @@ if not os.path.exists(posts_dir):
 # 레포지토리 로드
 repo = git.Repo(repo_path)
 
+# 🔹 원격 저장소와 동기화 추가
+origin = repo.remote(name='origin')
+origin.pull('main')  # 원격 저장소의 최신 상태 반영
+
 # RSS 피드 파싱
 feed = feedparser.parse(rss_url)
 
 # 각 글을 파일로 저장하고 커밋
 for entry in feed.entries:
-    # 파일 이름에서 유효하지 않은 문자 제거 또는 대체
-    file_name = entry.title
-    file_name = file_name.replace('/', '-')  # 슬래시를 대시로 대체
-    file_name = file_name.replace('\\', '-')  # 백슬래시를 대시로 대체
-    # 필요에 따라 추가 문자 대체
-    file_name += '.md'
+    file_name = entry.title.replace('/', '-').replace('\\', '-') + '.md'
     file_path = os.path.join(posts_dir, file_name)
 
     # 파일이 이미 존재하지 않으면 생성
     if not os.path.exists(file_path):
         with open(file_path, 'w', encoding='utf-8') as file:
-            file.write(entry.description)  # 글 내용을 파일에 작성
+            file.write(entry.description)
 
         # 깃허브 커밋
         repo.git.add(file_path)
         repo.git.commit('-m', f'Add post: {entry.title}')
 
 # 변경 사항을 깃허브에 푸시
-repo.git.push()
+origin.push('main')
